9999502f193649d38143d809e40e5dd8fbb2ce68
Jonathan Keane <jkeane@gmail.com>
2021-03-19 16:32:52 -0500

    Redact slashes

--- tests/testthat/test-ensure-lib.R
+++ tests/testthat/test-ensure-lib.R
@@ -3,6 +3,11 @@ test_that("lib_dir()", {
     lib_dir("foo"),
     file.path(getwd(), "r_libs", paste0("R-", paste0(c(getRversion()$major, getRversion()$minor), collapse = ".")), "foo")
   )
+
+  expect_identical(
+    lib_dir("remote-user/arrow@branch/with/slashes"),
+    file.path(getwd(), "r_libs", paste0("R-", paste0(c(getRversion()$major, getRversion()$minor), collapse = ".")), "remote-user_arrow@branch_with_slashes")
+  )
 })
 
 test_that("identify_repo_ref()", {
