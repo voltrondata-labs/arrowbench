5f456bca647cf34f0cf9a0fd92023fb5276baa58
Weston Pace <weston.pace@gmail.com>
2021-05-21 13:27:56 -1000

    Removed the options reset clause since each benchmark gets its own process

--- R/bm-remote-dataset.R
+++ R/bm-remote-dataset.R
@@ -23,8 +23,6 @@ remote_dataset <- Benchmark("remote_dataset",
     stopifnot(
       "The dimensions do not match" = all.equal(dim(result), expected_dim)
     )
-    # Restore default value in case local benchmarks are going to run later
-    options("arrow.use_async" = FALSE)
   },
   packages_used = function(params) {
     c("arrow")
