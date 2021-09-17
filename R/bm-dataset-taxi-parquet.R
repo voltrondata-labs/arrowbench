2b4f68dd5da7a19a062acb3e06aa23d654042920
Jonathan Keane <jkeane@gmail.com>
2021-09-17 12:17:52 -0500

    shuffle some queries, add use_async

--- R/bm-dataset-taxi-parquet.R
+++ R/bm-dataset-taxi-parquet.R
@@ -5,18 +5,23 @@
 #'
 #' @export
 dataset_taxi_parquet <- Benchmark("dataset_taxi_parquet",
-  setup = function(query = names(dataset_taxi_parquet$cases)) {
+                                  setup = function(
+                                    query = names(dataset_taxi_parquet$cases),
+                                    use_async = c(TRUE, FALSE)
+                                  ) {
     library("dplyr")
     dataset <- ensure_dataset("taxi_parquet")
     query <- dataset_taxi_parquet$cases[[match.arg(query)]]
 
     BenchEnvironment(
       query = query,
-      dataset = dataset
+      dataset = dataset,
+      use_async = use_async
     )
   },
   before_each = {
     result <- NULL
+    options("arrow.use_async" = use_async)
   },
   run = {
     result <- query$query(dataset)
@@ -94,6 +99,136 @@ dataset_taxi_parquet <- Benchmark("dataset_taxi_parquet",
       },
       assert = function(result) {
         stopifnot("dims does not match" = identical(result, c(1547741381L, 20L)))
+      }
+    ),
+
+
+    vignette_alt = list(
+      query = function(ds) {
+        ds %>%
+          filter(total_amount > 100, year == 2015) %>%
+          select(tip_amount, total_amount, passenger_count) %>%
+          collect() %>%
+          group_by(passenger_count) %>%
+          summarize(
+            tip_pct = median(100 * tip_amount / total_amount),
+            n = n()
+          )
+      },
+      assert = function(result) {
+        stopifnot(
+          identical(dim(result), c(10L, 3L)),
+          identical(names(result), c("passenger_count", "tip_pct", "n")),
+          identical(sum(result$n), 200807L)
+        )
+      }
+    ),
+    payment_type_3_alt = list(
+      query = function(ds) {
+        ds %>%
+          filter(payment_type == "3") %>%
+          select(year, month, passenger_count) %>%
+          collect() %>%
+          group_by(year, month) %>%
+          summarize(
+            total_passengers = sum(passenger_count, na.rm = TRUE),
+            n = n()
+          )
+      },
+      assert = function(result) {
+        stopifnot(
+          identical(dim(result), c(54L, 4L)),
+          identical(names(result), c("year", "month", "total_passengers", "n")),
+          identical(sum(result$n), 2412399L)
+        )
+      }
+    ),
+    # The intention of this is to filter + read from a small number of parquet
+    # files (smaller than the number of threads) to see if parallelism is
+    # beneficial
+    small_no_files_alt = list(
+      query = function(ds) {
+        ds %>%
+          filter(total_amount > 20, year %in% c(2011, 2019) & month == 2) %>%
+          select(tip_amount, total_amount, passenger_count) %>%
+          collect() %>%
+          group_by(passenger_count) %>%
+          summarize(
+            tip_pct = median(100 * tip_amount / total_amount),
+            n = n()
+          )
+      },
+      assert = function(result) {
+        stopifnot(
+          identical(dim(result), c(11L, 3L)),
+          identical(names(result), c("passenger_count", "tip_pct", "n")),
+          identical(sum(result$n), 3069271L)
+        )
+      }
+    ),
+
+
+    vignette_new = list(
+      query = function(ds) {
+        ds %>%
+          filter(total_amount > 100, year == 2015) %>%
+          select(tip_amount, total_amount, passenger_count) %>%
+          group_by(passenger_count) %>%
+          summarize(
+            tip_pct = mean(100 * tip_amount / total_amount),
+            n = n()
+          ) %>%
+          collect()
+      },
+      assert = function(result) {
+        stopifnot(
+          identical(dim(result), c(10L, 3L)),
+          identical(names(result), c("passenger_count", "tip_pct", "n")),
+          identical(sum(result$n), 200807L)
+        )
+      }
+    ),
+    payment_type_3_new = list(
+      query = function(ds) {
+        ds %>%
+          filter(payment_type == "3") %>%
+          select(year, month, passenger_count) %>%
+          group_by(year, month) %>%
+          summarize(
+            total_passengers = sum(passenger_count, na.rm = TRUE),
+            n = n()
+          ) %>%
+          collect()
+      },
+      assert = function(result) {
+        stopifnot(
+          identical(dim(result), c(54L, 4L)),
+          identical(names(result), c("year", "month", "total_passengers", "n")),
+          identical(sum(result$n), 2412399L)
+        )
+      }
+    ),
+    # The intention of this is to filter + read from a small number of parquet
+    # files (smaller than the number of threads) to see if parallelism is
+    # beneficial
+    small_no_files_new = list(
+      query = function(ds) {
+        ds %>%
+          filter(total_amount > 20, year %in% c(2011, 2019) & month == 2) %>%
+          select(tip_amount, total_amount, passenger_count) %>%
+          group_by(passenger_count) %>%
+          summarize(
+            tip_pct = mean(100 * tip_amount / total_amount),
+            n = n()
+          ) %>%
+          collect()
+      },
+      assert = function(result) {
+        stopifnot(
+          identical(dim(result), c(11L, 3L)),
+          identical(names(result), c("passenger_count", "tip_pct", "n")),
+          identical(sum(result$n), 3069271L)
+        )
       }
     )
   ),
