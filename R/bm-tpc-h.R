67025392607201ccb52768a6f03dbffa0114e8a6
Dewey Dunnington <dewey@fishandwhistle.net>
2021-11-16 09:26:33 -0400

    number 20 is still wrong but much cleaner

--- R/bm-tpc-h.R
+++ R/bm-tpc-h.R
@@ -1163,26 +1163,23 @@ tpc_h_queries[[20]] <- function(input_func) {
     ) %>%
     select(s_suppkey, s_name, s_address)
 
-  partsupp_ca <- input_func("partsupp") %>%
-    semi_join(supplier_ca, c("ps_suppkey" = "s_suppkey"))
-
   part_forest <- input_func("part") %>%
     filter(grepl("^forest", p_name))
+
+  partsupp_forest_ca <- input_func("partsupp") %>%
+    semi_join(supplier_ca, c("ps_suppkey" = "s_suppkey")) %>%
+    semi_join(part_forest, by = c("ps_partkey" = "p_partkey"))
 
   qty_threshold <- input_func("lineitem") %>%
     filter(
       l_shipdate >= as.Date("1994-01-01"),
       l_shipdate < as.Date("1995-01-01")
     ) %>%
-    inner_join(
-      partsupp_ca %>%
-        semi_join(part_forest, by = c("ps_partkey" = "p_partkey")),
-      by = c("l_partkey" = "ps_partkey")
-    ) %>%
+    inner_join(partsupp_forest_ca, by = c("l_partkey" = "ps_partkey")) %>%
     group_by(l_partkey, ps_suppkey) %>%
     summarise(qty_threshold = 0.5 * sum(l_quantity), .groups = "drop")
 
-  partsupp_ca_filtered <- partsupp_ca %>%
+  partsupp_forest_ca_filtered <- partsupp_forest_ca %>%
     inner_join(
       qty_threshold,
       by = c("ps_suppkey", "ps_partkey" = "l_partkey")
@@ -1190,7 +1187,7 @@ tpc_h_queries[[20]] <- function(input_func) {
     filter(ps_availqty > qty_threshold)
 
   supplier_ca %>%
-    semi_join(partsupp_ca_filtered, by = c("s_suppkey" = "ps_suppkey")) %>%
+    semi_join(partsupp_forest_ca_filtered, by = c("s_suppkey" = "ps_suppkey")) %>%
     select(s_name, s_address) %>%
     arrange(s_name) %>%
     collect()
