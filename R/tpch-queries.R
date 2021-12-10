631e0625c7fda26e2acc955cba4a50adfdcf564a
Jonathan Keane <jkeane@gmail.com>
2021-12-10 11:01:28 -0800

    Use a single query for tpch-22 (#62)

--- R/tpch-queries.R
+++ R/tpch-queries.R
@@ -717,19 +717,19 @@ tpc_h_queries[[21]] <- function(input_func, collect_func = dplyr::collect) {
 }
 
 tpc_h_queries[[22]] <- function(input_func, collect_func = dplyr::collect) {
-  acctbal_min <- input_func("customer") %>%
+  acctbal_mins <- input_func("customer") %>%
     filter(
       substr(c_phone, 1, 2) %in% c("13", "31", "23", "29", "30", "18", "17") &
         c_acctbal > 0
     ) %>%
-    summarise(mean(c_acctbal, na.rm = TRUE)) %>%
-    collect( )
+    summarise(acctbal_min = mean(c_acctbal, na.rm = TRUE), join_id = 1L)
 
   input_func("customer") %>%
-    mutate(cntrycode = substr(c_phone, 1, 2)) %>%
+    mutate(cntrycode = substr(c_phone, 1, 2), join_id = 1L) %>%
+    left_join(acctbal_mins, by = "join_id") %>%
     filter(
       cntrycode %in% c("13", "31", "23", "29", "30", "18", "17") &
-        c_acctbal > acctbal_min[[1]]
+        c_acctbal > acctbal_min
     ) %>%
     anti_join(input_func("orders"), by = c("c_custkey" = "o_custkey")) %>%
     select(cntrycode, c_acctbal) %>%
