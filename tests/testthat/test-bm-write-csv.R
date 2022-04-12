98a5a2f6d731afe8883daa8f51f3f30ecaeb69b0
SAm Albers <sam.albers@gmail.com>
2022-04-12 13:41:02 -0700

    add test for write_csv

---
+++ tests/testthat/test-bm-write-csv.R
@@ -0,0 +1,12 @@
+test_that("write_csv benchmark works", {
+  expect_s3_class(
+    run_benchmark(
+      write_csv,
+      source = "nyctaxi_sample",
+      writer = c("arrow", "data.table", "vroom", "readr", "base")
+      ), "arrowbench_results"
+  )
+})
+
+
+
