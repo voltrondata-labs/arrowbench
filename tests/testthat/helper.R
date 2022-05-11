667e0603152cbdc9f0496ca99551f156a5264c03
Sam Albers <sam.albers@gmail.com>
2022-05-11 16:27:41 -0700

    no need to get rid of bm-scripts folder anymore

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,6 +1,3 @@
 library(jsonlite)
 
-wipe_results <- function() {
-  unlink(test_path("results/"), recursive = TRUE)
-  unlink(test_path("bm-scripts/"), recursive = TRUE)
-}
+wipe_results <- function() unlink(test_path("results/"), recursive = TRUE)
