740318859778e2ec6730f95c9c970e050990cc38
Edward Visel <1693477+alistaire47@users.noreply.github.com>
2022-05-18 21:04:37 +0000

    Parameterize rbinding

--- R/bm-read-json.R
+++ R/bm-read-json.R
@@ -5,6 +5,7 @@
 #' * `reader` One of `c("arrow", "jsonlite", "ndjson", "RcppSimdJson")`
 #' * `compression` One of `c("uncompressed", "gzip")`
 #' * `output_format` One of `c("arrow_table", "data_frame")`
+#' * `rbinder` Method for simplifying to dataframe. Not relevant for {arrow} and {ndjson}.
 #'
 #' @export
 #' @importFrom R.utils gzip
@@ -13,10 +14,21 @@ read_json <- Benchmark(
   setup = function(source = names(known_sources),
                    reader = c("arrow", "jsonlite", "ndjson", "RcppSimdJson"),
                    compression = c("uncompressed", "gzip"),
-                   output_format = c("arrow_table", "data_frame")) {
+                   output_format = c("arrow_table", "data_frame"),
+                   rbinder = c("package", "dplyr", "data.table", "base")) {
     reader <- match.arg(reader)
     compression <- match.arg(compression)
     output_format <- match.arg(output_format)
+    rbinder <- match.arg(rbinder)
+
+    rbind_func <- switch(
+      rbinder,
+      package = identity,
+      dplyr = dplyr::bind_rows,
+      data.table = data.table::rbindlist,
+      # Warning! Base solution will break if ndjson has implicit nulls or varying field order
+      base = function(...) do.call(rbind.data.frame, ...)
+    )
 
     input_file <- ensure_format(source, "json", compression)
 
@@ -25,14 +37,17 @@ read_json <- Benchmark(
       read_func = get_json_reader(reader),
       input_file = input_file,
       result_dim = get_source_attr(source, "dim"),
-      as_data_frame = output_format == "data_frame"
+      as_data_frame = output_format == "data_frame",
+      rbinder = rbinder,
+      rbind_func = rbind_func
     )
   },
   before_each = {
     result <- NULL
   },
   run = {
-    result <- read_func(input_file, as_data_frame = as_data_frame)
+    result <- read_func(input_file, rbinder = rbinder, as_data_frame = as_data_frame)
+    result <- rbind_func(result)
   },
   after_each = {
     result_class <- if (as_data_frame) "data.frame" else c("Table", "ArrowObject")
@@ -51,15 +66,24 @@ read_json <- Benchmark(
   },
   valid_params = function(params) {
     drop <- ( params$output_format == "arrow_table" & params$reader != "arrow" ) |
-      ( params$reader == "jsonify" & params$compression == "gzip")
+      ( params$reader == "jsonify" & params$compression == "gzip" ) |
+      ( params$rbinder != "package" & params$reader %in% c("arrow", "ndjson") ) |
+      ( params$rbinder == "package" & params$reader == "RcppSimdJson")
     params[!drop, ]
   },
   packages_used = function(params) {
-    if ('RcppSimdJson' %in% params$reader) {
-      c(params$reader, 'data.table')
-    } else {
-      params$reader
-    }
+    packages <- params$reader
+
+    for (rbinder in params$rbinder)
+      packages <- c(packages, switch(
+        rbinder,
+        default = NULL,
+        dplyr = "dplyr",
+        data.table = "data.table",
+        base = NULL
+      ))
+
+    unique(packages)
   }
 )
 
@@ -82,22 +106,32 @@ get_json_reader <- function(reader) {
 
   reader_functions <- list(
     arrow = arrow::read_json_arrow,
-    jsonlite = function(path, ...) {
+    jsonlite = function(path, rbinder, ...) {
       con <- get_con(path)
       on.exit(close(con))
-      jsonlite::stream_in(con = con, verbose = FALSE)
+      out <- jsonlite::stream_in(
+        con = con,
+        simplifyVector = rbinder == "package",
+        simplifyMatrix = FALSE,
+        simplifyDataFrame = rbinder == "package",
+        verbose = FALSE
+      )
+      # no option to use NA instead of NULL, and `do.call(rbind.data.frame, ...)` can't handle the latter
+      if (rbinder == "base") {
+        out <- lapply(out, function(x) lapply(x, function(y) if (is.null(y)) NA else y))
+      }
+      out
     },
-    ndjson = function(..., as_data_frame) ndjson::stream_in(...),
+    ndjson = function(..., rbinder, as_data_frame) ndjson::stream_in(...),
     # NOTE: Removed jsonify from default options because it's unstable
-    jsonify = function(path, ..., as_data_frame) {
+    jsonify = function(path, rbinder, ..., as_data_frame) {
       con <- get_con(path)
       on.exit(close(con))
-      jsonify::from_ndjson(con, ...)
+      jsonify::from_ndjson(con, simplify = rbinder == "package", ...)
     },
-    RcppSimdJson = function(path, ..., as_data_frame) {
+    RcppSimdJson = function(path, ..., rbinder, as_data_frame) {
       lines <- readLines(path)
       json <- RcppSimdJson::fparse(lines, single_null = NA, max_simplify_lvl = 3L, ...)
-      data.table::rbindlist(json)
     }
   )
 
