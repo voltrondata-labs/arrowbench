1c9cf6d509d5aa85133834926535fde487f9939b
Jonathan Keane <jkeane@gmail.com>
2022-09-23 10:44:35 -0500

    gc after each iteration?

--- tests/testthat/test-datalogistik.R
+++ tests/testthat/test-datalogistik.R
@@ -25,7 +25,6 @@ sources_to_test <- sources_to_test[!sources_to_test %in% c("tpch")]
 for (format in c("parquet", "csv")) {
   for (source in sources_to_test) {
     test_that(paste0("datalogistik transition: ", source, ", ", format), {
-      if (source == "chi_traffic_2020_Q1") skip_on_ci("chi_traffic_2020_Q1 is too large for GHA, apparently")
       if (source == "chi_traffic_2020_Q1" && format == "csv") skip("chi_traffic_2020_Q1 can't be saved as a csv")
       if (source == "type_simple_features" && format == "csv") skip("type_simple_features can't be saved as a csv")
       if (source == "type_nested" && format == "csv") skip("type_nested can't be saved as a csv")
@@ -39,6 +38,10 @@ for (format in c("parquet", "csv")) {
         tab <- get_read_function(format)(source_file, as_data_frame = FALSE)
       }
       expect_identical(dim(tab), dims)
+
+      rm(tab)
+      gc(full = TRUE)
+      print(arrow_info())
     })
   }
 }
